281357dbb23bfe40a184f1b782fd50f099d8a544
Andy Callahan <amandafillpot@icloud.com>
2018-07-03 13:19:55 -0700

    day 1 takehome

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,6 @@
-//DECLARE A VARIABLE
-var firstName;
-
-//DEFINE YOUR VARIABLE
-firstName = "Andy"; //String
-
-//DEFINE AND DECLARE YOUR VARIABLE
-var lastName = "Callahan";
-
-console.log(firstName, lastName);
-
-//SINGLE LINE COMMENT
-
-/* MULTI LINE COMMENT
-firstName = 'Amanda';
-console.log(firstName);
-*/
-
-var getTweetDataAsync //This gets data from the Twitter
-
-{
-  //New Scope.
-  var myName;
-}
+// DAY 1 TAKEHOME
+var stringStatement = "\tHello, my name is Andy and I am \
+here to learn JavaScript. My favorite hobby is gaming and \
+figuring out ways to convert my favorite video games into \
+Dungeons and Dragons themes.";
+console.log(stringStatement);
