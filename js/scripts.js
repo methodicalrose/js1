a74f16bb88e860be2e0529dd40d37ceea79603bc
Andy Callahan <amandafillpot@icloud.com>
2018-07-02 21:00:57 -0700

    end of day 1

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var myString = "Hello World!*76";
+console.log(typeof myString); // String
+
+var myNumber = 43;
+console.log(typeof myNumber); // Number
+
+var isAlive = true; // true-false, also 1-0(CAUTION: may read as a number instead)
+console.log(typeof isAlive); // Boolean
+
+var newVariable;
+console.log(typeof newVariable); // Undefined
+
+isLoggedIn = null; // null is the absence of value
