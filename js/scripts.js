e963ae97e7f64c0ff3153671af489ddd55d2c56f
Andy Callahan <amandafillpot@icloud.com>
2018-07-02 20:37:23 -0700

    day 1 inclass work

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+//DECLARE A VARIABLE
+var firstName;
+
+//DEFINE YOUR VARIABLE
+firstName = "Andy"; //String
+
+//DEFINE AND DECLARE YOUR VARIABLE
+var lastName = "Callahan";
+
+console.log(firstName, lastName);
+
+//SINGLE LINE COMMENT
+
+/* MULTI LINE COMMENT
+firstName = 'Amanda';
+console.log(firstName);
+*/
+
+var getTweetDataAsync //This gets data from the Twitter
+
+{
+  //New Scope.
+  var myName;
+}
