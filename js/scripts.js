ffee813c22087f1ad0ed305084513a83d5ae486f
Andy Callahan <amandafillpot@icloud.com>
2018-07-10 16:49:11 -0700

    day 3 takehome

--- js/scripts.js
+++ js/scripts.js
@@ -4,33 +4,33 @@ let favColor = prompt("What's your favorite color? Choose red, orange, yellow, g
 
 switch(favColor.toLowerCase()) {
   case "red":
-    alert(`${name}'s favorite color is red.`);
+    alert(`${name} is a fiery inferno.`);
     break;
   case "orange":
-    alert(`${name}'s favorite color is orange.`);
+    alert(`${name} REALLY likes popsicles. And dreams.`);
     break;
   case "yellow":
-    alert(`${name}'s favorite color is yellow.`);
+    alert(`Mellow ${name}'s secret wish is to live in a submarine.`);
     break;
   case "green":
-    alert(`${name}'s favorite color is green.`);
+    alert(`Maybe ${name} should take up gardening.`);
     break;
   case "blue":
-    alert(`${name}'s favorite color is blue.`);
+    alert(`${name} just wants to sit in the rain and enjoy the feeling of water slowly filling their galoshes.`);
     break;
   case "pink":
-    alert(`${name}'s favorite color is pink.`);
+    alert(`${name}'s favorite activites are writing lip-gloss messages on mirrors and bedazzling the ass of their Victoria's Secret sweatpants.`);
     break;
   case "purple":
-    alert(`${name}'s favorite color is purple.`);
+    alert(`Nice choice, ${name}. You have a beautiful soul.`);
     break;
   case "black":
-    alert(`${name}'s favorite color is black.`);
+    alert(`Just like ${name}'s heart. Or the color of the void. Or both. (Spoiler alert: ${name}'s heart IS the void.)`);
     break;
   case "white":
-    alert(`${name}'s favorite color is white.`);
+    alert(`${name} is as pure and clean as freshly fallen snow. Well, aren't you an angel?`);
     break;
   default:
-    alert(`Come on, ${name}, stop trying to be original.`);
+    alert(`Are you serious? Come on, ${name}, stop playin'.`);
     break;
 }
