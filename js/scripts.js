9b8988aabf015f94a2d8ff13d3799a2032b8bc3c
Andy Callahan <amandafillpot@icloud.com>
2018-07-16 21:03:06 -0700

    day 5 work

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+// ANONYMOUS FUNCTION
+//FUNCTION DEFINITION (NOT Invocation)
+// function (string) { //Inside the () is where the ARGUMENTS/PARAMETERS go
+//   return string.toUpperCase(); //Data transformation and return
+// }; //End of the function
+
+//NAMED FUNCTIONS
+//FUNCTION DEFINITION
+// function addTwo(num) {
+//   return num + 2;
+// };
+
+//CALL the Function
+// console.log(addTwo(4)); //You can check the results of your FUNCTION
+//
+// let newNum = addTwo(4); //You can use your function as a variable value
+
+// function logMyString(string) {
+//   console.log(string); //Does not Return... does nothing outside of this function
+// };
+// logMyString('I am the best.'); //Nothing gets logged
+//
+// function alertsAd() { //You can have a function without parameters
+//   alert('We have a sale');
+// };
+// alertsAd(); //Calls the function - no values necessary
+
+//RECURSIVE FUNCTIONS
+// let i = 0;
+// function iCallMyself() {
+//   console.log(i);
+//   i++;
+//   iCallMyself();
+// }; //---DON'T DO IT!!! It will call itself and cause an infinite loop
+
+//FAT ARROW FUNCTIONS
+//----BASIC IMPLEMENTATION
+(logFunction, alertFunction) => { //NO Function keyword
+  logFunction('Hello world');
+  alertFunction('Goodbye, friends!');
+};
+
+//----ONLY 1 ARGUMENT
+string => { //Parentheses are optional
+  console.log(string);
+};
+
+//----FUNCTION EXPRESSION OF FAT ARROW
+let logString = string => { //Function Expressions allow functions to be named
+  console.log(string);
+};
+logString('Hello cats!');
+
+//----IMPLICIT RETURN
+string => (string.toUpperCase()); //Automatically returns
+string => string.toUpperCase(); //Outer parentheses are optional
